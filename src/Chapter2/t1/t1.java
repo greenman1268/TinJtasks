8171da3d74fdb143994dd6ee14ddbb1a4ce8fff2
Grisha <grisha-chapliy@rambler.ru>
2016-01-24 11:03:00 +0200

    Initial commit

---
+++ src/Chapter2/t1/t1.java
@@ -0,0 +1,30 @@
+package Chapter2.t1;
+
+/**
+ * Created by Grisha on 24.01.2016.
+ */
+//: Chapter/t1/t1.java
+
+import java.util.*;
+
+/**
+ * Прога с джава-док
+ *
+ * @author Iam
+ * @author www.mysite.ua
+ * @version 1.0
+ */
+public class t1 {
+    /**
+     * Enter to the class
+     *
+     * @param args massive
+     */
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println(new Date());
+    }
+} /*Output:(55% match)
+hello
+Sun Jan 24 10:46:33 EET 2016
+*///:~
