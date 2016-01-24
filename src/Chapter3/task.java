7755b478b697f7c9e621adf1c8ab32f7ec96ac24
Grisha <grisha-chapliy@rambler.ru>
2016-01-24 18:01:28 +0200

    new

---
+++ src/Chapter3/task.java
@@ -0,0 +1,15 @@
+package Chapter3;
+
+import java.util.Random;
+
+/**
+ * Created by Grisha on 24.01.2016.
+ */
+public class task {
+
+    public static void main(String[] args) {
+
+        System.out.println((int)23.3);
+    }
+
+}
