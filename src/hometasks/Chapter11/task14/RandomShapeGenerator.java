d119386001a6b26e341e364a669fce002b6c43f5
Admin <grisha-chapliy@rambler.ru>
2016-02-29 12:22:01 +0200

    chapter11 task14 page358

--- src/hometasks/Chapter11/task14/RandomShapeGenerator.java
+++ src/hometasks/Chapter11/task14/RandomShapeGenerator.java
@@ -3,27 +3,38 @@ package hometasks.Chapter11.task14;
 /**
  * Created by Admin on 26.02.2016.
  */
-import javax.swing.text.html.HTMLDocument;
+//358
+
 import java.util.*;
 
-public class RandomShapeGenerator implements Iterator<Shape>
-{
-    private int size = 0;
-    Random random = new Random(47);
-    public RandomShapeGenerator(int k) {
-    this.size = k;
+public class RandomShapeGenerator implements Iterable<Shape>
+{   private Random random = new Random(47);
+    private List<Shape> list;
+
+    public RandomShapeGenerator(){
+        list = new ArrayList<>();
+        list.add(new Circle());
+        list.add(new Square());
+        list.add(new Triangle());
+
     }
 
     @Override
-    public boolean hasNext() {
-        return size>0;
-    }
+    public Iterator<Shape> iterator() {
+        return new Iterator<Shape>() {
+            int index = 0;
+            @Override
+            public boolean hasNext() {
+                return index<10;
+            }
 
-    @Override
-    public Shape next() {
-        Shape s;
-        while (hasNext())
-        return null;
+            @Override
+            public Shape next() {
+                index++;
+                return list.get(random.nextInt(3));
+
+            }
+        };
     }
 }
 
@@ -56,5 +67,15 @@ class Triangle extends Shape{
     }
     public void erase(){
         System.out.println("Triangle.erase");
+    }
+}
+
+class Test{
+
+    public static void main(String[] args) {
+        RandomShapeGenerator rand = new RandomShapeGenerator();
+       for (Shape p : rand){
+           p.draw();
+       }
     }
 }
