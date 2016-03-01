7f65c807465a5b49122ad6fe3de41b37ce427abb
Grisha <grisha-chapliy@rambler.ru>
2016-03-01 22:04:38 +0200

    Chapter 13 page436

--- src/hometasks/Chapter13/task9/Regular.java
+++ src/hometasks/Chapter13/task9/Regular.java
@@ -4,4 +4,7 @@ package hometasks.Chapter13.task9;
  * Created by Grisha on 01.03.2016.
  */
 public class Regular {
+    public static void main(String[] args) {
+        String[] regex = {"^Java","\\Breg.*",};
+    }
 }
